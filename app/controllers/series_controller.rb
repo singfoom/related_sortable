de0784f39dd4fe6407667ad59dd058d3d8d26563
Colin Campbell <singfoom@gmail.com>
2013-10-30 16:23:07 -0700

    Allowing series to create/edit one dependent story

--- app/controllers/series_controller.rb
+++ app/controllers/series_controller.rb
@@ -15,10 +15,12 @@ class SeriesController < ApplicationController
   # GET /series/new
   def new
     @series = Series.new
+    @series.stories.build
   end
 
   # GET /series/1/edit
   def edit
+    @series.stories.build
   end
 
   # POST /series
@@ -69,6 +71,7 @@ class SeriesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def series_params
-      params.require(:series).permit(:name)
+      params.require(:series).permit(:name, :stories, :stories_attributes => [
+      :id, :series_id, :_destroy, :description, :position, :title])
     end
 end
--- app/models/series.rb
+++ app/models/series.rb
@@ -1,3 +1,4 @@
 class Series < ActiveRecord::Base
   has_many :stories
+  accepts_nested_attributes_for :stories, allow_destroy: true
 end
